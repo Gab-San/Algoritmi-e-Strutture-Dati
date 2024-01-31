9cf651579736270269821a8fdc012dafaa6fc49c
Gab-San <santandreagabriele@gmail.com>
2024-01-31 12:05:43 +0100

    Aggiunta di alcuni commenti

--- Algoritmi/Merge-Sort.c
+++ Algoritmi/Merge-Sort.c
@@ -17,14 +17,16 @@ void Max(int* array, int len){
 
 void Merge(int* array, int startingIndex, int middleIndex, int lastIndex){
 
+    // Calcolo della lunghezza delle partizioni
     int len_array_1 = middleIndex - startingIndex;
     int len_array_2 = lastIndex - middleIndex;
+    // Costruzione di partizioni tramite array ausiliari
     int *aux_array_1 = (int*) malloc(sizeof(int) * (len_array_1 + 1));
-    int *aux_array_2 = (int*) malloc(sizeof(int) * (len_array_2 + 1));
-    // Da 0 a 9
     for(int i= 0; i < len_array_1; i++){
         aux_array_1[i] = array[startingIndex + i];
     }
+
+    int *aux_array_2 = (int*) malloc(sizeof(int) * (len_array_2 + 1));
     // Da 10 a 19
     for(int i = 0; i < len_array_2; i++){
         aux_array_2[i] = array[middleIndex + i];
@@ -37,16 +39,20 @@ void Merge(int* array, int startingIndex, int middleIndex, int lastIndex){
 
     int i = 0;
     int j = 0;
+    // Riordinamento dell'array
+    // Si noti che nel caso di due elementi basta scambiare tra loro gli elementi
     for(int k = startingIndex; k < lastIndex){
         if(aux_array_1[i] <= aux_array_2[j] ){
             array[k] = aux_array_1[i];
             i++;
         } else {
-            array[k] = aux_array_2[j]; //0 1 2 3 4 5 6 7 8 9 10
+            array[k] = aux_array_2[j]; 
             j++;
         }
     }
 
+    free(aux_array_1);
+    free(aux_array_2);
 
 }
 
@@ -57,7 +63,9 @@ void Merge_Sort(int* array, int startingIndex, int lastIndex){
         int middleIndex = (startingIndex + lastIndex + 1) / 2;
 
         // Divide et Impera: divide in due l'array
+        // Analisi della prima partizione dell'array
         Merge_Sort(array, startingIndex, middleIndex);
+        // Analisi della seconda partizione dell'array
         Merge_Sort(array, middleIndex +1, lastIndex);
         // Rimette assieme gli array partizionati
         Merge(array, startingIndex, middleIndex, lastIndex);
